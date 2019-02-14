c873de02edba25f12f0f432e8f2f6466621c8dbe
Chanwut Kittivorawong <chanwutk@gmail.com>
2019-02-14 01:41:30 -0800

    -

--- setup_db.js
+++ setup_db.js
@@ -8,7 +8,13 @@ const client = new Client({
 
 client.connect();
 
-client.query("DROP TABLE IF EXISTS previous_hour; CREATE TABLE previous_hour (hour INTEGER);", (err, res) => {
+client.query("DROP TABLE IF EXISTS previous_hour;", (err, res) => {
+  if (err) throw err;
+  console.log(res.rows);
+  client.end();
+});
+
+client.query("CREATE TABLE previous_hour (hour INTEGER); ", (err, res) => {
   if (err) throw err;
   console.log(res.rows);
   client.end();
